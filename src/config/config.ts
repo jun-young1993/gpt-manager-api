491e502b9ddb199438052f906749edd67a284365
kim-jun-young <jykim@gemiso.com>
2023-07-10 18:19:51 +0900

    변환

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,5 @@
-import {GoogleGeoCode} from "../google-trends/google-trends.interface";
-import {CreateChatCompletionRequest} from "openai";
+import { GoogleGeoCode } from '../google-trends/google-trends.interface';
+import { CreateChatCompletionRequest } from 'openai';
 
 export const _GUEST = 'guest';
 export const guestUser = {
@@ -11,14 +11,21 @@ export const guestUser = {
 };
 
 export const prompts = {
-  article : (title:string, url:string, geo: GoogleGeoCode): CreateChatCompletionRequest => {
+  article: (
+    title: string,
+    url: string,
+    geo: GoogleGeoCode,
+  ): CreateChatCompletionRequest => {
     return {
       model: 'gpt-3.5-turbo',
-      messages : [
-        { "role" : "system", "content": `As a blog writer, please write a blog post in the language associated with the country code (${geo}) using the'article title(URL)'.` },
-        { "role": "user", "content": `${title}(${url})` }
-      ]
-    }
-  }
-
-}
+      messages: [
+        {
+          role: 'system',
+          // content: `As a blog writer, please write a blog post in the language associated with the country code (${geo}) using the'article title(URL)'.`,
+          content: `As a blog writer, please write a blog post in the language associated with the country code (${geo}) using the 'article title(URL)'. Also, responses will be provided using HTML syntax: 1. <strong>A sentence you want to emphasize</strong> 2.Line break <br> 3.<strong className='underline decoration-sky-500/30'> An important sentence </strong>`,
+        },
+        { role: 'user', content: `${title}(${url})` },
+      ],
+    };
+  },
+};
